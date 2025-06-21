d1867497b1604d79063c52a77c6b3d76f7f55289
Vincent Fretin <vincent.fretin@gmail.com>
2025-06-21 13:42:01 +0200

    Allow to reparent or reorder an entity, createEntity can now create children if defined in the definition (fix #26)

--- src/components/scenegraph/Entity.js
+++ src/components/scenegraph/Entity.js
@@ -15,6 +15,11 @@ import { removeEntity, cloneEntity } from '../../lib/entity';
 import EntityRepresentation from '../EntityRepresentation';
 import Events from '../../lib/Events';
 
+// Utility function to check if entity is a container (including scene)
+export const isContainer = (entity) => {
+  return entity.tagName === 'A-SCENE';
+};
+
 export default class Entity extends React.Component {
   static propTypes = {
     id: PropTypes.string,
@@ -24,7 +29,17 @@ export default class Entity extends React.Component {
     isFiltering: PropTypes.bool,
     isSelected: PropTypes.bool,
     selectEntity: PropTypes.func,
-    toggleExpandedCollapsed: PropTypes.func
+    toggleExpandedCollapsed: PropTypes.func,
+    // Drag and drop props
+    draggedEntity: PropTypes.object,
+    setDraggedEntity: PropTypes.func,
+    hoveredDropTarget: PropTypes.object,
+    setHoveredDropTarget: PropTypes.func,
+    insertionInfo: PropTypes.object,
+    setInsertionInfo: PropTypes.func,
+    onReparentEntity: PropTypes.func,
+    canBeDragged: PropTypes.func,
+    canBeDropTarget: PropTypes.func
   };
 
   onClick = () => this.props.selectEntity(this.props.entity);
@@ -41,11 +56,124 @@ export default class Entity extends React.Component {
     });
   };
 
+  // Drag and drop handlers
+  onDragStart = (e) => {
+    if (!this.props.canBeDragged(this.props.entity)) {
+      e.preventDefault();
+      return;
+    }
+
+    this.props.setDraggedEntity(this.props.entity);
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/plain', '');
+
+    // Hide the drag ghost by setting a transparent image
+    const emptyImg = new Image();
+    emptyImg.src =
+      'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
+    e.dataTransfer.setDragImage(emptyImg, 0, 0);
+  };
+
+  onDragEnd = () => {
+    this.props.setDraggedEntity(null);
+    this.props.setHoveredDropTarget(null);
+    this.props.setInsertionInfo(null);
+  };
+
+  onDragOver = (e) => {
+    if (
+      !this.props.canBeDropTarget(this.props.entity, this.props.draggedEntity)
+    ) {
+      return;
+    }
+
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+
+    const rect = e.currentTarget.getBoundingClientRect();
+    const y = e.clientY;
+    const topQuarter = rect.top + rect.height * 0.25;
+    const bottomQuarter = rect.bottom - rect.height * 0.25;
+
+    let position = 'child';
+
+    // Don't allow before/after for scene entities
+    if (!isContainer(this.props.entity)) {
+      if (y <= topQuarter) {
+        // Check if this "before" position would be redundant
+        const draggedEntity = this.props.draggedEntity;
+        const prevSibling = this.props.entity.previousElementSibling;
+
+        // Don't show "before" if the dragged entity is the previous sibling
+        if (draggedEntity !== prevSibling) {
+          position = 'before';
+        }
+      } else if (y >= bottomQuarter) {
+        // Check if this "after" position would be redundant
+        const draggedEntity = this.props.draggedEntity;
+        const nextSibling = this.props.entity.nextElementSibling;
+
+        // Don't show "after" if the dragged entity is the next sibling
+        if (draggedEntity !== nextSibling) {
+          position = 'after';
+        }
+      }
+    }
+
+    this.props.setHoveredDropTarget(this.props.entity);
+    this.props.setInsertionInfo({ entity: this.props.entity, position });
+  };
+
+  onDragLeave = (e) => {
+    // Only clear hover state if we're leaving this element entirely
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX;
+    const y = e.clientY;
+
+    if (x < rect.left || x > rect.right || y < rect.top || y > rect.bottom) {
+      this.props.setHoveredDropTarget(null);
+      this.props.setInsertionInfo(null);
+    }
+  };
+
+  onDrop = (e) => {
+    e.preventDefault();
+    const insertion = this.props.insertionInfo;
+    this.props.setHoveredDropTarget(null);
+    this.props.setInsertionInfo(null);
+
+    const dragged = this.props.draggedEntity;
+    if (
+      dragged &&
+      this.props.canBeDropTarget(this.props.entity, dragged) &&
+      insertion
+    ) {
+      this.props.onReparentEntity(
+        dragged,
+        insertion.entity,
+        insertion.position
+      );
+    }
+  };
+
   render() {
     const isFiltering = this.props.isFiltering;
     const isExpanded = this.props.isExpanded;
     const entity = this.props.entity;
     const tagName = entity.tagName.toLowerCase();
+
+    // Drag and drop state
+    const isDragging = this.props.draggedEntity === entity;
+    const isHoveredDropTarget =
+      this.props.hoveredDropTarget === entity &&
+      this.props.canBeDropTarget(entity, this.props.draggedEntity);
+    const insertionPosition =
+      this.props.insertionInfo && this.props.insertionInfo.entity === entity
+        ? this.props.insertionInfo.position
+        : null;
+
+    // Check if entity can be dragged
+    const canBeDragged = this.props.canBeDragged(entity);
 
     // Clone and remove buttons if not a-scene.
     const cloneButton =
@@ -109,11 +237,26 @@ export default class Entity extends React.Component {
       active: this.props.isSelected,
       entity: true,
       novisible: !visible,
-      option: true
+      option: true,
+      // Drag and drop classes
+      dragging: isDragging,
+      'drop-before': isHoveredDropTarget && insertionPosition === 'before',
+      'drop-after': isHoveredDropTarget && insertionPosition === 'after',
+      'drop-child': isHoveredDropTarget && insertionPosition === 'child'
     });
 
     return (
-      <div className={className} onClick={this.onClick} id={this.props.id}>
+      <div
+        className={className}
+        onClick={this.onClick}
+        id={this.props.id}
+        draggable={canBeDragged}
+        onDragStart={this.onDragStart}
+        onDragEnd={this.onDragEnd}
+        onDragOver={this.onDragOver}
+        onDragLeave={this.onDragLeave}
+        onDrop={this.onDrop}
+      >
         <span>
           {visibilityButton}
           <span
